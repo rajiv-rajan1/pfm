d492ce40ae7b84454d323f4b11c1b43ffa72c593
udhay rajeev <udhay1co.de@gmail.com>
2025-12-22 13:21:30 +0530

    frontend

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,4 +3,4 @@
 # It usually runs automatically in /docker-entrypoint.d/ for nginx image, 
 # but we are explicitly placing it there.
 
-envsubst '${BACKEND_API_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_API_URL} ${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
