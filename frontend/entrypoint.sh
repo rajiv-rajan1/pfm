43e64a65453aeda41e4d1de99fc0c17f87e71057
udhay rajeev <udhay1co.de@gmail.com>
2025-12-22 15:48:44 +0530

    frontend and backend comm

--- frontend/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script injects environment variables into the Nginx config
-# It usually runs automatically in /docker-entrypoint.d/ for nginx image, 
-# but we are explicitly placing it there.
-
-envsubst '${BACKEND_API_URL} ${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
